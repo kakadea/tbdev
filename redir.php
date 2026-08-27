ae9286744d687b1655a4ae486169a7b5de7fc054
kakadea <9.464279e+06+kakadea@users.noreply.github.com>
2026-08-27 02:41:37 +0000

    fix: harden external redirect handler

--- redir.php
+++ redir.php
@@ -10,30 +10,35 @@
 |   svn: http://sourceforge.net/projects/tbdevnet/
 |   Licence Info: GPL
 +------------------------------------------------
-|   $Date$
-|   $Revision$
-|   $Author$
-|   $URL$
-+------------------------------------------------
 */
 require_once "include/bittorrent.php";
 dbconn(false);
 
-if(!isset($CURUSER)) 
-	die();
-	
-  $url = '';
-  while (list($var,$val) = each($_GET))
-    $url .= "&$var=$val";
-	if(preg_match( "/([<>'\"]|&#039|&#33;|&#34|%27|%22|%3E|%3C|&#x27|&#x22|&#x3E|&#x3C|\.js)/i", $url ))
-		header("Location: http://www.urbandictionary.com/define.php?term=twat");
-$i = strpos($url, "&url=");
-if ($i !== false)
-	$url = substr($url, $i + 5);
-if (substr($url, 0, 4) == "www.")
-	$url = "http://" . $url;
-	if (strlen($url) < 10) die();
-  print("<html><head><meta http-equiv='refresh' content='3;url=$url'></head><body>\n");
-  print("<div style='width:100%;text-align:center;background: #E9D58F;border: 1px solid #CEAA49;margin: 5px 0 5px 0;padding: 0 5px 0 5px;font-weight: bold;'>Redirecting you to:<br />\n");
-  print(htmlsafechars($url)."</div></body></html>\n");
+if (!$CURUSER)
+  exit;
+
+$raw_url = isset($_GET['url']) ? trim((string) $_GET['url']) : '';
+if ($raw_url === '' || preg_match('/[\x00-\x1F\x7F]/', $raw_url))
+  httperr();
+
+if (stripos($raw_url, 'www.') === 0)
+  $raw_url = 'http://' . $raw_url;
+
+$parts = parse_url($raw_url);
+if ($parts === false || empty($parts['scheme']) || empty($parts['host']) ||
+    !in_array(strtolower($parts['scheme']), array('http', 'https'), true) ||
+    isset($parts['user']) || isset($parts['pass']) || isset($parts['fragment']))
+  httperr();
+
+$url = filter_var($raw_url, FILTER_VALIDATE_URL);
+if ($url === false)
+  httperr();
+
+$safe_url = htmlspecialchars($url, ENT_QUOTES, 'UTF-8');
+header('Referrer-Policy: no-referrer');
+header('Content-Security-Policy: default-src \'none\'; style-src \'unsafe-inline\'');
+header('X-Content-Type-Options: nosniff');
+print("<html><head><meta http-equiv='refresh' content='3;url={$safe_url}'></head><body>\n");
+print("<div style='width:100%;text-align:center;background:#E9D58F;border:1px solid #CEAA49;margin:5px 0;padding:0 5px;font-weight:bold;'>Redirecting you to:<br />\n");
+print(htmlsafechars($url) . "</div></body></html>\n");
 ?>
