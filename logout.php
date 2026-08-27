bf697b8d6c8ea941b218dc6077375fe1fad2bde1
kakadea <9.464279e+06+kakadea@users.noreply.github.com>
2026-08-27 03:12:22 +0000

    fix: destroy PHP session on logout

--- logout.php
+++ logout.php
@@ -16,10 +16,21 @@
 |   $URL$
 +------------------------------------------------
 */
-require_once("include/bittorrent.php");
+require_once('include/bittorrent.php');
+
+security_session_start();
+if (session_status() === PHP_SESSION_ACTIVE)
+{
+    $_SESSION = array();
+    if (ini_get('session.use_cookies'))
+    {
+        $params = session_get_cookie_params();
+        setcookie(session_name(), '', time() - 42000, $params['path'], $params['domain'], (bool) $params['secure'], (bool) $params['httponly']);
+    }
+    session_destroy();
+}
 
 dbconn();
-
 logoutcookie();
 
 //header("Refresh: 0; url=./");
